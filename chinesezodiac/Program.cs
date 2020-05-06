7bbe094d2517fd734afc35313f9a8e6c8ea26cdb
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-06 23:13:31 +0900

    05 문제

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,53 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+            String[] ddi = { "원숭이", "닭", "개", "돼지", "쥐", "소", "호랑이", "토끼", "용", "뱀", "말", "양" };
+            Console.Write("태어난 년도: ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ddi[input % 12] + "띠");
+
+            /* 노가다
+            switch (input % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이띠");
+                    break;
+                case 1:
+                    Console.WriteLine("닭띠");
+                    break;
+                case 2:
+                    Console.WriteLine("개띠");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지띠");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐띠");
+                    break;
+                case 5:
+                    Console.WriteLine("소띠");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이띠");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼띠");
+                    break;
+                case 8:
+                    Console.WriteLine("용띠");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀띠");
+                    break;
+                case 10:
+                    Console.WriteLine("말띠");
+                    break;
+                case 11:
+                    Console.WriteLine("양띠");
+                    break;
+            }
+            */
         }
     }
 }
